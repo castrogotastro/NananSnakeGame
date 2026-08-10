956553fc66ce320a4f956ef21bc1fa5f33e5829a
Reid G <reid.m.gillis@gmail.com>
2026-08-10 15:00:15 -0700

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include "Utilities.h"
+
+
+int main()
+{
+	bool isGameOver = true;
+
+	do
+	{
+		LOG("Start Game");
+
+	} while (isGameOver);
+
+	LOG("Game Over");
+}
---
+++ Utilities.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+#define LOG(X) std::cout << X 
+#define LOG_LN(X) std::cout << X << std::endl
