81bbf2870f0a00d5429ab51c8e842337a8008049
Reid G <reid.m.gillis@gmail.com>
2026-08-11 11:20:23 -0700

    Add Getter functions. Make properties private

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -4,3 +4,18 @@ GameBoard::GameBoard(int InWidth, int InHeight)
 	:mWidth(InWidth), mHeight(InHeight)
 {
 }
+
+int GameBoard::GetWidth() const
+{
+	return mWidth;
+}
+
+int GameBoard::GetHeight() const
+{
+	return mHeight;
+}
+
+char GameBoard::GetWallIcon() const
+{
+	return mIconWall;
+}
--- GameBoard.h
+++ GameBoard.h
@@ -4,8 +4,14 @@ class GameBoard
 public:
 	GameBoard(int InWidth, int InHeight);
 
+private:
 	int mWidth;
 	int mHeight;
 	char mIconWall = '#';
+
+public:
+	int GetWidth() const;
+	int GetHeight() const;
+	char GetWallIcon() const;
 };
 
