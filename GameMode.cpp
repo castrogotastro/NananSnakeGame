370947a99148207d29992988b6952281c1ba70a6
Reid G <reid.m.gillis@gmail.com>
2026-08-13 11:39:48 -0700

    Functionality for tracking win loose state and player state

--- GameMode.cpp
+++ GameMode.cpp
@@ -1,4 +1,7 @@
 #include "GameMode.h"
+#include "PlayerState.h"
+#include "GameBoard.h"
+#include "Snake.h"
 
 GameMode* GameMode::sInstance = nullptr;
 
@@ -13,4 +16,71 @@ GameMode* GameMode::GetInstace()
 		sInstance = new GameMode();
 	}
 	return sInstance;
+}
+
+void GameMode::InitializeTrackingProperties(
+	const std::shared_ptr<Snake>& InSnakeShared, 
+	const std::shared_ptr<GameBoard>& InGameBoardShared, 
+	const std::shared_ptr<PlayerState>& InPlayerStateShared)
+{
+	mSnakeWeak = InSnakeShared;
+	mGameboardWeak = InGameBoardShared;
+	mPlayerStateWeak = InPlayerStateShared;
+}
+
+const GameMatchStates& GameMode::GetGameMatchState() const
+{
+	return mGameMatchState;
+}
+
+void GameMode::SetGameMatchState(GameMatchStates InGameMatchState)
+{
+	mGameMatchState = InGameMatchState;
+}
+
+bool GameMode::GetWinState()
+{
+	return mHasPlayerWon;
+}
+
+bool GameMode::GetLossState()
+{
+	return mHasPlayerLost;
+}
+
+void GameMode::ResetStates()
+{
+	mHasPlayerLost = false;
+	mHasPlayerWon = false;
+}
+
+void GameMode::UpdateWinState()
+{
+	if (std::shared_ptr<PlayerState> playerStateShared = mPlayerStateWeak.lock())
+	{
+		if (std::shared_ptr<GameBoard> gameBoardShared = mGameboardWeak.lock())
+		{
+			if (playerStateShared->GetScore() >= gameBoardShared->GetScoreToWinLevel())
+			{
+				mHasPlayerWon = true;
+			}
+		}
+	}
+}
+
+void GameMode::UpdateLooseState()
+{
+	if (std::shared_ptr<Snake> snakeShared = mSnakeWeak.lock())
+	{
+		if (std::shared_ptr<GameBoard> gameBoardShared = mGameboardWeak.lock())
+		{
+			if (snakeShared->GetLocation().mX == 0
+				|| snakeShared->GetLocation().mX == gameBoardShared->GetWidth()
+				|| snakeShared->GetLocation().mY == 0
+				|| snakeShared->GetLocation().mY == gameBoardShared->GetHeight())
+			{
+				mHasPlayerLost = true;
+			}
+		}
+	}
 }
--- GameMode.h
+++ GameMode.h
@@ -1,17 +1,53 @@
 #pragma once
 
 #include <memory>
+#include <string>
 
 class Snake;
 class GameBoard;
+class PlayerState;
+
+//Main Menu
+	//Start game
+	//Exit Game
+
+
+
+//Game Start (level 1, 2, 3)
+
+	//Win level
+	//Win the game
+	//Loose the level
+
+//Restart menu
+
+enum class GameMatchStates
+{
+	MenuMain,
+	BeginMatch,
+	RestartMatch,
+	MatchStart,
+	MatchEnd,
+	MenuRestart,
+	MenuWin,
+	MenuNextLevel,
+	MenuGameOver,
+};
 
 class GameMode
 {
-	//properties
+	//properties - Singleton
 	static GameMode* sInstance;
-	std::weak_ptr<Snake> mSnakePlayerWeak;
-	std::weak_ptr<GameBoard> mGameboardWeak;
 
+	//Properties for tracking win and loose state
+	std::weak_ptr<Snake> mSnakeWeak;
+	std::weak_ptr<GameBoard> mGameboardWeak;
+	std::weak_ptr<PlayerState> mPlayerStateWeak;
+	bool mHasPlayerWon = false;
+	bool mHasPlayerLost = false;
+
+	//Properties for tracking game match state (where player is in game)
+	GameMatchStates mGameMatchState = GameMatchStates::MenuMain;
 
 private:
 	//constructors & assigment operators
@@ -23,6 +59,25 @@ private:
 public:
 	static GameMode* GetInstace();
 
+	void InitializeTrackingProperties(
+		const std::shared_ptr<Snake>& InSnakeShared,
+		const std::shared_ptr<GameBoard>& InGameBoardShared,
+		const std::shared_ptr<PlayerState>& InPlayerStateShared);
 
+	//Getters and Setters
+	const GameMatchStates& GetGameMatchState() const;
+	void SetGameMatchState(GameMatchStates InGameMatchState);
+
+	bool GetWinState();
+	bool GetLossState();
+	void ResetStates();
+
+	//Update Win and Loose states 
+		// WIN - when score equals the value needed to win a level
+		//Loose - when the player location equals the boarder
+				//TODO also when the player location equals the tail
+				//also if player head location equals a bomb or a bullet.
+	void UpdateWinState();
+	void UpdateLooseState();
 };
 
