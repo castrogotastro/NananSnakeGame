46b7b2712fdc72d4139919a9e2a800209e779c07
Reid G <reid.m.gillis@gmail.com>
2026-08-13 17:16:29 -0700

    made single param parameterized constructor explicit

--- HUDGameplay.h
+++ HUDGameplay.h
@@ -7,7 +7,7 @@ class PlayerState;
 class HUDGameplay : public HUD
 {
 public:
-	HUDGameplay(const std::shared_ptr<PlayerState>& InPlayerStateShared);
+	explicit HUDGameplay(const std::shared_ptr<PlayerState>& InPlayerStateShared);
 
 private:
 	std::weak_ptr<PlayerState> mPlayerStateWeak;
